ccbab1355de8920c72c4d377de216abd3300ee6e
esneidermendez <esneidermendez@soyyo.co>
2021-10-08 12:22:05 -0500

    Ajustes de web lockerUmb

--- mvc/connection.php
+++ mvc/connection.php
@@ -12,7 +12,7 @@ class Db
 	public static function  getConnect(){
 		if (!isset(self::$instance)) {
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
-			self::$instance= new PDO('mysql:host=localhost;dbname=fraterniapp','root','',$pdo_options);
+			self::$instance= new PDO('mysql:host=heroku_3b0fafbdc4f5d9f;dbname=heroku_0922ccf225472e1','b9dda9592d64cb','4c4d3caa',$pdo_options);
 		} 
 		return self::$instance;
 	}
